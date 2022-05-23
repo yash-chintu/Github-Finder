8fcf3647610f9a5990bd497152c0e80bf771a58f
unknown <krishnayaswanth4@gmail.com>
2022-05-23 19:15:29 +0530

    One Module Done

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+return (
+    <>
+        <h1 className='text-6xl mb-4'>Github Finder</h1>
+        <p className='mb-4 text-2xl font-light'>
+            A React app to search GitHub profiles and see profile details using Github REST API . 
+        </p>
+        <p>This
+            project is done as a part of the React Front To Back Udemy course by Brad Traversey .
+        </p>
+    </>
+)
+}
+
+export default About
